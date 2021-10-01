85d59f2860bd430f6972b6d12719565dd5d5c3ca
Lorenzo Giorgini <lorenzo.giorgini@outlook.com>
2021-10-01 17:53:56 +0200

    fixed bugs
    now the loader and error message works

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import MyNavbar from './components/MyNavbar';
-import DropDown from './components/DropDown';
-import MyGalleries from './components/MyGalleries';
-import MyFooter from './components/MyFooter';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import MyNavbar from "./components/MyNavbar";
+import DropDown from "./components/DropDown";
+import MyGalleries from "./components/MyGalleries";
+import MyFooter from "./components/MyFooter";
+import React from "react";
 
-function App() {
-  return (
-    <div>
-      <MyNavbar />
-      <DropDown />
-      <MyGalleries query={"avengers"}/>
-      <MyGalleries query={"harry potter"}/>
-      <MyGalleries query={"batman"}/>
-      <MyFooter />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    query: "",
+  };
+
+  render() {
+    return (
+      <div>
+        <MyNavbar />
+        <DropDown />
+        <div>
+          <input
+            type="text"
+            placeholder="Search..."
+            onChange={(e) => this.setState({ query: e.target.value })}
+          />
+          {/* <input
+          type="button"
+          value="Search"
+          onClick={() => this.fetchedData()}
+        /> */}
+        </div>
+        {this.state.query.length > 0 ? (
+          <MyGalleries query={this.state.query} />
+        ) : (
+          <>
+            <MyGalleries query={"avengers"} />
+            <MyGalleries query={"harry potter"} />
+            <MyGalleries query={"batman"} />
+          </>
+        )}
+
+        <MyFooter />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/MyGalleries.jsx
+++ src/components/MyGalleries.jsx
@@ -1,75 +1,67 @@
-import {Component} from "react"
-import SingleMovies from "./SingleMovies"
-import MySpinner from "./MySpinner"
-import Alert from "react-bootstrap/Alert"
+import { Component } from "react";
+import SingleMovies from "./SingleMovies";
+import MySpinner from "./MySpinner";
+import Alert from "react-bootstrap/Alert";
 
 class MyGalleries extends Component {
+  state = {
+    movies: [],
+    
+    loading: false,
+    errorMsg: ""
+  };
 
+  fetchedData = async () => {
+    try {
+      this.setState({ loading: true });
+      const response = await fetch(
+        "http://www.omdbapi.co/?apikey=e59f0763&s=" + this.props.query
+      );
+      const data = await response.json();
+      console.log(data);
+      if(data.Search) {
+        this.setState({ movies: data.Search, loading: false, errorMsg: "" });
+      }
+    
+    } catch (error) {
 
-    state = {
-        movies: [
-
-        ],
-        query: this.props.query,
-        loading: false,
-        errorMsg: ""
+      this.setState({ errorMsg: "not found" });
     }
+  };
 
+  async componentDidMount() {
+    this.fetchedData();
+  }
 
+  componentDidUpdate(prevProps) {
+      if(prevProps.query!==this.props.query) {
+        this.fetchedData();
+      }
+  }
 
-    fetchedData = async () => {
-        try {
-            this.setState({loading: true})
-            const response = await fetch( "http://www.omdbapi.com/?apikey=e59f0763&s=" + this.state.query);
-            const data = await response.json()
-            console.log(data)
-            this.setState({ movies: data, loading: false, errorMsg: "" })
-        } catch(error) {
-            this.setState({errorMsg: error.message})
-        }
-    }
-
-
-    async componentDidMount() {
-        this.fetchedData()
-    }
-
-
-
-    render() {
-        return (
-            <>
-                <div>
-                    <input type="text" placeholder="Search..." onChange={(e) => this.setState({query: e.target.value})}/>
-                    <input type="button" value="Search" onClick={() => this.fetchedData()} />
-                </div>
-                
-                <div className=".container-fluid pl-4">
-                    <h1 className="title shows-title" id="recent">
-                        {this.state.query}
-                    </h1>
-                    <div className="row">
-                        {
-                            this.state.errorMsg && 
-                            <Alert variant="warning">
-                                Cannot load the data: {this.state.errorMsg}
-                            </Alert>
-                            
-                        }
-                        {
-                            this.state.loading && <MySpinner />
-                        }
-                        {
-                            this.state.movies.Search &&
-                            this.state.movies.Search.map((movie , i) => 
-                                <SingleMovies key={i} image={movie.Poster} />
-                            )
-                        }
-                    </div>
-                </div>
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <div className=".container-fluid pl-4">
+          <h1 className="title shows-title" id="recent">
+            {this.state.query}
+          </h1>
+          <div className="row">
+            {this.state.errorMsg && (
+              <Alert variant="warning">
+                Cannot load the data: {this.state.errorMsg}
+              </Alert>
+            )}
+            {this.state.loading && <MySpinner />}
+            {
+              this.state.movies.map((movie, i) => (
+                <SingleMovies key={i} image={movie.Poster} />
+              ))}
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
-export default MyGalleries
+export default MyGalleries;
