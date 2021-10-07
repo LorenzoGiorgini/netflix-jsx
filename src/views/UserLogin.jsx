29a8b63b23b60170709f92813d4bcc5e8b26e855
Lorenzo Giorgini <lorenzo.giorgini@outlook.com>
2021-10-07 22:06:34 +0200

    Update UserLogin.jsx

--- src/views/UserLogin.jsx
+++ src/views/UserLogin.jsx
@@ -6,6 +6,7 @@ import { Col, Row } from "react-bootstrap"
 
 const UserLogin = () => {
 
+
     const [user , setUser] = useState({
         name: "",
         surname: "",
@@ -15,73 +16,86 @@ const UserLogin = () => {
     })
 
 
+    let [completed , setCompleted] = useState(false)
+
+
+
+
     const checkIfFormIsCompleted = () => user.name.length >= 3 && user.surname.length >= 3 && user.email.length >= 5 && user.password.length >= 8 && user.confirmPassword === user.password
 
 
     return(
         <>
-        <div className="spacer">
-            <Row style={{marginTop: "100px" , color: "white"}}>
-                <Col>
-                    <Form>
-                        <Form.Group className="mb-3" controlId="formBasicName">
-                            <Form.Label>Name</Form.Label>
-                            <Form.Control 
-                            type="text" 
-                            placeholder="Enter Name" 
-                            value={user.name}
-                            onChange={(e) => setUser({...user , name: e.target.value})}
-                            />
-                        </Form.Group>
+        {
+        completed === true ? (
+            <div className="spacer">
+                <h1 style={{margin: "auto" , color: "white"}}>hudaidhkbjaskjd</h1>
+            </div>
+        ) : (
+            <div className="spacer">
+                <Row style={{marginTop: "200px" , color: "white"}}>
+                    <Col>
+                        <Form>
+                            <Form.Group className="mb-3" controlId="formBasicName">
+                                <Form.Label>Name</Form.Label>
+                                <Form.Control 
+                                type="text" 
+                                placeholder="Enter Name" 
+                                value={user.name}
+                                onChange={(e) => setUser({...user , name: e.target.value})}
+                                />
+                            </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="formBasicName">
-                            <Form.Label>Surname</Form.Label>
-                            <Form.Control 
-                            type="text" 
-                            placeholder="Enter Surname" 
-                            value={user.surname}
-                            onChange={(e) => setUser({...user , surname: e.target.value})}
-                            />
-                        </Form.Group>
+                            <Form.Group className="mb-3" controlId="formBasicName">
+                                <Form.Label>Surname</Form.Label>
+                                <Form.Control 
+                                type="text" 
+                                placeholder="Enter Surname" 
+                                value={user.surname}
+                                onChange={(e) => setUser({...user , surname: e.target.value})}
+                                />
+                            </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control 
-                            type="email" 
-                            placeholder="Enter Email" 
-                            value={user.email}
-                            onChange={(e) => setUser({...user , email: e.target.value})}
-                            />
-                        </Form.Group>
+                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control 
+                                type="email" 
+                                placeholder="Enter Email" 
+                                value={user.email}
+                                onChange={(e) => setUser({...user , email: e.target.value})}
+                                />
+                            </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control 
-                            type="password" 
-                            placeholder="Password" 
-                            value={user.password}
-                            onChange={(e) => setUser({...user , password: e.target.value})}
-                            />
-                        </Form.Group>
+                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control 
+                                type="password" 
+                                placeholder="Password" 
+                                value={user.password}
+                                onChange={(e) => setUser({...user , password: e.target.value})}
+                                />
+                            </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>Confirm Password</Form.Label>
-                            <Form.Control 
-                            type="password" 
-                            placeholder="Confirm Password" 
-                            value={user.confirmPassword}
-                            onChange={(e) => setUser({...user , confirmPassword: e.target.value})}
-                            />
-                        </Form.Group>
-                        
-                        <Button variant="primary" type="submit" disabled={!checkIfFormIsCompleted()} onClick={(e) => e.preventDefault()}>
-                            Submit
-                        </Button>
-                        
-                    </Form>
-                </Col>
-            </Row>
-        </div>
+                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                                <Form.Label>Confirm Password</Form.Label>
+                                <Form.Control 
+                                type="password" 
+                                placeholder="Confirm Password" 
+                                value={user.confirmPassword}
+                                onChange={(e) => setUser({...user , confirmPassword: e.target.value})}
+                                />
+                            </Form.Group>
+                            
+                            <Button variant="primary" disabled={!checkIfFormIsCompleted()} onClick={() => setCompleted(true)}>
+                                Submit
+                            </Button>
+
+                        </Form>
+                    </Col>
+                </Row>
+            </div>
+        )
+        }
         </>
     )
 }
